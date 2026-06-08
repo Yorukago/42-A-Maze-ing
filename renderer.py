16325cb80040603ce05ee4391c74881ea85f3428
Lucas Jeronimo O'Neill Mendes <ljeronim@c1r7s5.42lisboa.com>
2026-06-08 19:55:04 +0100

    added window

--- renderer.py
+++ renderer.py
@@ -1,12 +1,15 @@
 from mlx import Mlx
 
-class MlxUtils:
+class MlxUtils():
     def __init__(self, width: int, height: int):
         self.mlx: Mlx = Mlx()
         self.pmlx: any = self.mlx.mlx_init()
         self.pwin: any = self.mlx.mlx_new_window(self.pmlx, width, height, "A-Maze-ing")
 
+c
 
-mlx = MlxUtils(1000, 1000)
 
-    
+omlx = MlxUtils(1000, 1000)
+omlx.mlx.mlx_loop(omlx.pmlx)
+
+bgimg = omlx.mlx.mlx_new_image()
